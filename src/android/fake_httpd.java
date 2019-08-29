b13243c9bec76ff13225e5bbe37185e97e2464ef
Christophe Malek <jcmalek@lakeheadu.ca>
2019-08-29 14:19:12 -0400

    add TAG variable and rename LOG to Log

--- src/android/fake_httpd.java
+++ src/android/fake_httpd.java
@@ -24,20 +24,21 @@ import java.io.FileInputStream;
 */
 public class fake_httpd extends CordovaPlugin {
 
+    public static final String TAG = "fake_httpd";
     public static final String HTTP_PROTOCOL = "http";
     public static final String HTTPS_PROTOCOL = "https";
     public static final String LOCALHOST_AUTHORITY = "localhost";
 
     @Override
     public Uri remapUri(Uri uri) {
-        LOG.d(TAG, "remapUri(" + uri + ")");
+        Log.d(TAG, "remapUri(" + uri + ")");
         if (! (HTTP_PROTOCOL.equals(uri.getScheme()) || HTTPS_PROTOCOL.equals(uri.getScheme()) ) ) {
             return null;
         }
         if (! LOCALHOST_AUTHORITY.equals(uri.getAuthority())) {
             return null;
         }
-        LOG.d(TAG, "toPluginUri:  " + toPluginUri(uri));
+        Log.d(TAG, "toPluginUri:  " + toPluginUri(uri));
         // for handleOpenForRead to get called the Uri needs to be formatted as cdvplugin://pluginId/
         return toPluginUri(uri);
     }
@@ -46,16 +47,16 @@ public class fake_httpd extends CordovaPlugin {
     public CordovaResourceApi.OpenForReadResult handleOpenForRead(Uri uri) throws IOException {
         //Uri original_uri = fromPluginUri(uri);
         Uri file_uri = uri.buildUpon().scheme("file").authority("").path("/android_asset/www/" + uri.getPath()).build();
-        LOG.d(TAG, "file_uri:  " + file_uri);
-        LOG.d(TAG, "getPath:  " + file_uri.getPath());
+        Log.d(TAG, "file_uri:  " + file_uri);
+        Log.d(TAG, "getPath:  " + file_uri.getPath());
 
         FileInputStream input_stream = new FileInputStream(file_uri.getPath());
 
         Context context = this.cordova.getActivity().getApplicationContext();
         ContentResolver cR = context.getContentResolver();
 
-        LOG.d(TAG, "getType:  " + cR.getType(file_uri));
-        LOG.d(TAG, "size:  " + input_stream.getChannel().size());
+        Log.d(TAG, "getType:  " + cR.getType(file_uri));
+        Log.d(TAG, "size:  " + input_stream.getChannel().size());
 
         return new OpenForReadResult(uri, input_stream, cR.getType(file_uri), input_stream.getChannel().size(), null);
     }

84748db86a8ed636e22cff17a6979d40a0d200a2
Christophe Malek <jcmalek@lakeheadu.ca>
2019-09-01 14:10:45 -0400

    rename class cordova-plugin-fake-webserver to cordova_plugin_fake_webserver (dashes not allowed in class name)

--- src/android/cordova-plugin-fake-webserver.java
+++ src/android/cordova-plugin-fake-webserver.java
@@ -8,7 +8,7 @@ import android.util.Log;
 /**
 * This class remaps http://localhost and https://localhost URIs to file:///android_asset/www/
 */
-public class cordova-plugin-fake-webserver extends CordovaPlugin {
+public class cordova_plugin_fake_webserver extends CordovaPlugin {
 
     public static final String TAG = "cordova-plugin-fake-webserver";
     public static final String HTTP_PROTOCOL = "http";
